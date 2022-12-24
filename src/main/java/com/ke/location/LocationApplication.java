c199156e7aab2e09c37127bd1548b23af4195562
kiplimo-dev <kiplimocollins855@gmail.com>
2022-12-24 16:24:07 +0300

    added some features

--- src/main/java/com/ke/location/LocationApplication.java
+++ src/main/java/com/ke/location/LocationApplication.java
@@ -2,8 +2,9 @@ package com.ke.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class LocationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ke/location/entity/County.java
+++ src/main/java/com/ke/location/entity/County.java
@@ -1,10 +1,12 @@
 package com.ke.location.entity;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 
 @Getter
